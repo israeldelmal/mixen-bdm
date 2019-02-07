69f0d932aba64ec7b984b89d53ce946e1bdb842e
Israel <israelofevil@gmail.com>
2019-02-06 21:15:21 -0700

    Inicialización del proyecto

---
+++ index.php
@@ -0,0 +1,150 @@
+<?php include 'header.html'; ?>
+	<?php include 'nav.html'; ?>
+	<!-- Cabecera -->
+	<header id="inicio" data-parallax="scroll" data-image-src="assets/imagenes/cabecera/fondo.jpg">
+		<h1>Dedicados a la elaboración de
+		<strong>proyectos estructurales
+		de acero y concreto</strong></h1>
+		<a href="#"><i class="fas fa-play-circle"></i></a>
+		<span><a href="mailto:contacto@bdmgroup.com">contacto@bdmgroup.com</a></span>
+	</header>
+	<!-- Nosotros -->
+	<section id="nosotros">
+		<h1><span>Nosotros</span></h1>
+		<div>
+			<div>
+				<h2>BDM UTILIZA SOFTWARE DE VANGUARDIA
+				<strong>TANO PARA DISEÑO ESTRUCTURAL
+				COMO PARA MODELACIÓN</strong></h2>
+				<p>Llevamos de la mano al cliente desde la idea conceptual hasta la ejecución del proyecto.</p>
+				<p>Nuestros servicios son dirigidos a empreas Constructoras, despachos de arquitectura,
+				desarrolladores industriales, desarrollaodres comerciales y dependiencias de gobierno.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore architecto nisi ducimus hic, nesciunt ipsum quis officiis provident in dignissimos rerum placeat perferendis, consequatur sunt odio, dolores minima! Aliquam, repellat!</p>
+			</div>
+			<div>
+				<img src="assets/imagenes/nosotros/figura-1.jpg" alt="#">
+			</div>
+		</div>
+	</section>
+	<!-- Servicios -->
+	<section id="servicios" data-parallax="scroll" data-image-src="assets/imagenes/servicios/fondo.jpg">
+		<h1>Conoce más de <strong>nuestros servicios</strong></h1>
+		<div>
+			<a href="#">
+				<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+				<span>Diseño estructural</span>
+				<i class="fas fa-plus-circle"></i>
+			</a>
+			<a href="#">
+				<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+				<span>Modelado BIM</span>
+				<i class="fas fa-plus-circle"></i>
+			</a>
+			<a href="#">
+				<img src="assets/imagenes/servicios/figura-2.jpg" alt="#">
+				<span>Consultoría</span>
+				<i class="fas fa-plus-circle"></i>
+			</a>
+			<a href="#">
+				<img src="assets/imagenes/servicios/figura-3.jpg" alt="#">
+				<span>Peritaje</span>
+				<i class="fas fa-plus-circle"></i>
+			</a>
+		</div>
+	</section>
+	<!-- Empresas -->
+	<div id="empresas">
+		<div><img src="assets/imagenes/empresas/figura-1.png" alt="#"></div>
+		<div><img src="assets/imagenes/empresas/figura-2.png" alt="#"></div>
+		<div><img src="assets/imagenes/empresas/figura-3.png" alt="#"></div>
+		<div><img src="assets/imagenes/empresas/figura-4.png" alt="#"></div>
+		<div><img src="assets/imagenes/empresas/figura-5.png" alt="#"></div>
+		<div><img src="assets/imagenes/empresas/figura-6.png" alt="#"></div>
+	</div>
+	<!-- Descanso -->
+	<break data-parallax="scroll" data-image-src="assets/imagenes/descanso/fondo.jpg">
+		<div>
+			<h3>Ponte en contacto
+			<strong>En un click</strong></h3>
+		</div>
+		<div>
+			<a href="#">Contáctanos</a>
+		</div>
+	</break>
+	<!-- Proyectos -->
+	<main id="proyectos">
+		<h1>Conoce más de <strong>nuestros proyectos</strong></h1>
+		<sub>Nuestro personal a participado en la elaboracion de los siguientes proyectos:</sub>
+		<div>
+			<article>
+				<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+				<div>
+					<h1>Título del proyecto</h1>
+					<sub>Lorem ipsum dolor sit amet</sub>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere neque voluptate, veritatis eum dicta ab omnis totam laborum, illo eaque, quis suscipit quidem! Itaque in molestiae quisquam a illo.</p>
+					<a href="#">Ver más</a>
+				</div>
+			</article>
+			<article>
+				<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+				<div>
+					<h1>Título del proyecto</h1>
+					<sub>Lorem ipsum dolor sit amet</sub>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere neque voluptate, veritatis eum dicta ab omnis totam laborum, illo eaque, quis suscipit quidem! Itaque in molestiae quisquam a illo.</p>
+					<a href="#">Ver más</a>
+				</div>
+			</article>
+			<article>
+				<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+				<div>
+					<h1>Título del proyecto</h1>
+					<sub>Lorem ipsum dolor sit amet</sub>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere neque voluptate, veritatis eum dicta ab omnis totam laborum, illo eaque, quis suscipit quidem! Itaque in molestiae quisquam a illo.</p>
+					<a href="#">Ver más</a>
+				</div>
+			</article>
+			<article>
+				<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+				<div>
+					<h1>Título del proyecto</h1>
+					<sub>Lorem ipsum dolor sit amet</sub>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo facere neque voluptate, veritatis eum dicta ab omnis totam laborum, illo eaque, quis suscipit quidem! Itaque in molestiae quisquam a illo.</p>
+					<a href="#">Ver más</a>
+				</div>
+			</article>
+		</div>
+		<a href="#">Ver más proyectos</a>
+	</main>
+	<!-- Certificaciones -->
+	<section id="certificaciones" data-parallax="scroll" data-image-src="assets/imagenes/certificaciones/fondo.jpg">
+		<h1><span>Certificaciones</span></h1>
+		<div>
+			<figure>
+				<div>
+					<img src="assets/imagenes/certificaciones/figura-1.png" alt="#">
+				</div>
+				<figcaption><strong>BDM cuenta con personal calificado 
+				con mas de 10 años</strong> de experiencia 
+				en la elaboración de proyectos.</figcaption>
+			</figure>
+			<figure>
+				<div>
+					<img src="assets/imagenes/certificaciones/figura-2.png" alt="#">
+				</div>
+				<figcaption>En la división / área 
+				<strong>BIM la empresa cuenta con 
+				5 años de experiencia.</strong></figcaption>
+			</figure>
+			<figure>
+				<div>
+					<img src="assets/imagenes/certificaciones/figura-3.png" alt="#">
+				</div>
+				<figcaption>BDM cuenta con perito 
+				<strong>certificado ante el gobierno</strong> del 
+				estado de Chihuahua.</figcaption>
+			</figure>
+		</div>
+	</section>
+	<!-- Mapa -->
+	<map name="Ubicación"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.494594574547!2d-106.08191968496271!3d28.674847982401158!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea43dbc268a9a5%3A0xcdf3832f39950655!2sMixen%3A+Marketing+%26+Web+Technology!5e0!3m2!1ses-419!2smx!4v1549507532050" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe></map>
+<?php include 'footer.html'; ?>
---
+++ servicios.php
@@ -0,0 +1,62 @@
+<?php include 'header.html'; ?>
+	<?php include 'nav.html'; ?>
+	<!-- Cabecera -->
+	<services>
+		<header id="nosotros">
+			<div>
+				<figure><img src="assets/imagenes/cabecera/figura-1.jpg" alt="#"></figure>
+				<figure><img src="assets/imagenes/cabecera/figura-2.jpg" alt="#"></figure>
+				<figure><img src="assets/imagenes/cabecera/figura-3.jpg" alt="#"></figure>
+			</div>
+			<div>
+				<div>
+					<h1>Servicios:
+					<strong>Diseño estructural</strong></h1>
+					<ul>
+						<li>Lorem ipsum dolor sit amet.</li>
+						<li>Lorem ipsum dolor sit amet.</li>
+						<li>Lorem ipsum dolor sit amet.</li>
+						<li>Lorem ipsum dolor sit amet.</li>
+						<li>Lorem ipsum dolor sit amet.</li>
+					</ul>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus obcaecati deleniti similique maiores eligendi magnam, quidem id. Ratione expedita excepturi placeat quos error, quia dolore aperiam distinctio fugit. Harum, et.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus obcaecati deleniti similique maiores eligendi magnam, quidem id. Ratione expedita excepturi placeat quos error, quia dolore aperiam distinctio fugit. Harum, et.</p>
+				</div>
+				<div>
+					<img src="assets/imagenes/cabecera/figura-4.jpg" alt="#">
+				</div>
+			</div>
+		</header>
+		<!-- Servicios -->
+		<section id="servicios" data-parallax="scroll" data-image-src="assets/imagenes/servicios/fondo.jpg">
+			<h1>Conoce más de <strong>nuestros servicios</strong></h1>
+			<div class="triple">
+				<a href="#">
+					<img src="assets/imagenes/servicios/figura-1.jpg" alt="#">
+					<span>Modelado BIM</span>
+					<i class="fas fa-plus-circle"></i>
+				</a>
+				<a href="#">
+					<img src="assets/imagenes/servicios/figura-2.jpg" alt="#">
+					<span>Consultoría</span>
+					<i class="fas fa-plus-circle"></i>
+				</a>
+				<a href="#">
+					<img src="assets/imagenes/servicios/figura-3.jpg" alt="#">
+					<span>Peritaje</span>
+					<i class="fas fa-plus-circle"></i>
+				</a>
+			</div>
+		</section>
+		<!-- Descanso -->
+		<break  data-parallax="scroll" data-image-src="assets/imagenes/descanso/fondo.jpg">
+			<div>
+				<h3>Ponte en contacto
+				<strong>En un click</strong></h3>
+			</div>
+			<div>
+				<a href="#">Contáctanos</a>
+			</div>
+		</break>
+	</services>
+<?php include 'footer.html'; ?>
